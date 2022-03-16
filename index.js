459e42d745efb4e439020f1f2c9c066d3da9fa4e
Amro Abdalla <wed.kom@hotmail.com>
2022-03-16 16:05:23 -0500

    addition implementation working

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 console.log("hello world")
+
+const message = document.querySelector("#message")
+
+
+function addMovie(event) {
+    event.preventDefault()
+
+    const inputField = document.querySelector('input')
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click', crossOffMovie)
+    movie.appendChild(movieTitle)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    document.querySelector('ul').appendChild(movie)
+    inputField.value =" "
+}
+
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+    message.textContent=`${event.target.parentNode} deleted`
+    revealMessage()
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
+    if(event.target.classList.contains('checked')){
+        message.textContent= `${event.target.textContent} watched`
+    }else{
+        message.textContent =`${event.target.textContent} added back`
+    }
+    revealMessage()
+}
+
+function revealMessage(event){
+    // event.target.classList.toggle('hide')
+    setTimeout(() => { message.classList.add('hide')},2000)
+}
+
+document.querySelector('form').addEventListener('submit', addMovie)
+
+
